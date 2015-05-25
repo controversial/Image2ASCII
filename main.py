0dc9583cbc6a294c1fa366a82f010b016589dae9
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2015-05-25 13:26:34 -0400

    Delete main.py

--- main.py
+++
@@ -1,5 +0,0 @@
-from Image2ASCII import *
-image = Image.open("Test_Images/Calvin.jpg") #Open the image
-ascii = image2ASCII(image)                   #Convert the image to ASCII
-outputimage = RenderASCII(ascii)             #Render the ASCII
-outputimage.show()                           #Show the final product
